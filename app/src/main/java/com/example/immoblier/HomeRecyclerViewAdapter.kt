45213ab4d15472ff1e77472fb7d05d57fb1acf1d
hamza <fk_zaidi@esi.dz>
2019-06-12 23:58:22 +0100

    Added home view that contains the list of announcements. The view is not fully done yet.

---
+++ app/src/main/java/com/example/immoblier/HomeRecyclerViewAdapter.kt
@@ -0,0 +1,75 @@
+package com.example.immoblier
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import java.text.SimpleDateFormat
+
+class HomeRecyclerViewAdapter(data : ArrayList<Announcement>,internal var context : Context) : RecyclerView.Adapter<HomeRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layout = LayoutInflater.from(context).inflate(R.layout.announcement_item, parent, false)
+
+        return ViewHolder(layout)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+
+
+        holder.wilaya.text = data[position].wilaya
+        holder.address.text = data[position].address
+        holder.nbBedRooms.text = data[position].nbBedRooms.toString()
+        holder.nbBathRooms.text = data[position].nbBathRooms.toString()
+        holder.nbGarages.text = data[position].nbGarages.toString()
+        holder.type.text = data[position].type
+
+        val simpleDateFormat = SimpleDateFormat("dd-m-yyyy")
+
+        holder.date.text = simpleDateFormat.format(data[position].date)
+        holder.price.text = data[position].price.toString()
+        holder.housePicture.setImageResource(data[position].pictures[0])
+    }
+
+
+    internal var data : List<Announcement>
+
+    init {
+        this.data = data
+    }
+
+    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+
+        internal var housePicture : ImageView
+        internal var wilaya : TextView
+        internal var address : TextView
+        internal var nbBedRooms : TextView
+        internal var nbBathRooms : TextView
+        internal var nbGarages : TextView
+        internal var type : TextView
+        internal var date : TextView
+        internal var price : TextView
+
+        init {
+            housePicture = itemView.findViewById(R.id.iv_house_picture)
+            wilaya = itemView.findViewById(R.id.tvWilaya)
+            address = itemView.findViewById(R.id.tvAdress)
+            nbBedRooms = itemView.findViewById(R.id.tvBedRooms)
+            nbBathRooms = itemView.findViewById(R.id.tvBathroomNumber)
+            nbGarages = itemView.findViewById(R.id.tvGarageNumber)
+            type = itemView.findViewById(R.id.tvType)
+            date = itemView.findViewById(R.id.tvDate)
+            price = itemView.findViewById(R.id.tv_house_price)
+        }
+
+
+    }
+}
--- app/src/main/java/com/example/immoblier/MainActivity.kt
+++ app/src/main/java/com/example/immoblier/MainActivity.kt
@@ -2,11 +2,89 @@ package com.example.immoblier
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val announcements = ArrayList<Announcement>()
+
+
+        val firstHomePictures = ArrayList<Int>()
+        val secondHomePictures = ArrayList<Int>()
+        val thirdHomePictures = ArrayList<Int>()
+
+        firstHomePictures.add(R.drawable.images)
+        secondHomePictures.add(R.drawable.picture3)
+        thirdHomePictures.add(R.drawable.picture2)
+
+
+        announcements.add(Announcement(
+            "Alger",
+            "Bouchaoui 2 181",
+            "Villa à Bouchaoui",
+            "Villa",
+            3,
+            1,
+            1,
+            0,
+            1,
+            1,
+            75.0f,
+            firstHomePictures,
+            Agent("ZAIDI Moussa", "moussa@gmail.com", 744096126),
+            1000000f,
+            Date()
+        ))
+
+        announcements.add(Announcement(
+            "Alger",
+            "Bouchaoui 2 181",
+            "Villa à Bouchaoui",
+            "Villa",
+            3,
+            1,
+            1,
+            0,
+            1,
+            1,
+            75.0f,
+            secondHomePictures,
+            Agent("ZAIDI Moussa", "moussa@gmail.com", 744096126),
+            1000000f,
+            Date()
+        ))
+
+        announcements.add(Announcement(
+            "Alger",
+            "Bouchaoui 2 181",
+            "Villa à Bouchaoui",
+            "Villa",
+            3,
+            1,
+            1,
+            0,
+            1,
+            1,
+            75.0f,
+            thirdHomePictures,
+            Agent("ZAIDI Moussa", "moussa@gmail.com", 744096126),
+            1000000f,
+            Date()
+        ))
+
+
+
+        val announcementRecycler = findViewById<RecyclerView>(R.id.rvAnnouncements)
+        val adapter = HomeRecyclerViewAdapter(announcements, applicationContext)
+
+        announcementRecycler.layoutManager = LinearLayoutManager(applicationContext)
+        announcementRecycler.adapter = adapter
     }
 }

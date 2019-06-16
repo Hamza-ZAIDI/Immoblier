cdb340720cbd7ea4a40d0a398436992598b6fa30
hamza <fk_zaidi@esi.dz>
2019-06-16 20:03:00 +0100

    Added announcement detail layout

--- app/src/main/java/com/example/immoblier/HomeRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/immoblier/HomeRecyclerViewAdapter.kt
@@ -2,6 +2,7 @@ package com.example.immoblier
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.Intent
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -43,6 +44,29 @@ class HomeRecyclerViewAdapter(data : ArrayList<Announcement>,internal var contex
 
         holder.price.text = decimalFormat.format(data[position].price)
         holder.housePicture.setImageResource(data[position].pictures[0])
+
+        holder.detail.setOnClickListener{
+
+            val intent = Intent(context, AnnouncementDetailActivity ::class.java )
+            intent.putExtra("wilaya", data[position].wilaya)
+            intent.putExtra("adress", data[position].address)
+            intent.putExtra("bedrooms", data[position].nbBedRooms)
+            intent.putExtra("bathrooms", data[position].nbBathRooms)
+            intent.putExtra("garages",data[position].nbGarages)
+            intent.putExtra("type",data[position].type)
+            intent.putExtra("area",data[position].area)
+            intent.putExtra("price", data[position].price)
+            intent.putExtra("date", data[position].date)
+            intent.putExtra("description", data[position].description)
+            intent.putExtra("agentName", data[position].agent.fullName)
+            intent.putExtra("agentEmail", data[position].agent.email)
+            intent.putExtra("agentPhone",data[position].agent.phoneNumber)
+            intent.putExtra("kitchens", data[position].nbKitchens)
+            intent.putExtra("pools", data[position].nbPools)
+            intent.putExtra("gardens", data[position].nbGardens)
+            intent.putExtra("pictures", data[position].pictures)
+
+        }
     }
 
 
@@ -63,6 +87,7 @@ class HomeRecyclerViewAdapter(data : ArrayList<Announcement>,internal var contex
         internal var type : TextView
         internal var date : TextView
         internal var price : TextView
+        internal var detail : TextView
 
         init {
             housePicture = itemView.findViewById(R.id.iv_house_picture)
@@ -74,6 +99,7 @@ class HomeRecyclerViewAdapter(data : ArrayList<Announcement>,internal var contex
             type = itemView.findViewById(R.id.tvType)
             date = itemView.findViewById(R.id.tvDate)
             price = itemView.findViewById(R.id.tv_house_price)
+            detail = itemView.findViewById(R.id.tv_detail)
         }
 
 
